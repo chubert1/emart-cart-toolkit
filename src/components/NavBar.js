c3919a62fbb0da213f17548175aab53a2dbf833c
chubert1 <hubert.decroocq@laposte.net>
2022-08-18 20:11:55 +0200

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarLink,
+  MDBNavbarBrand,
+  MDBBadge,
+} from "mdb-react-ui-kit";
+import { useSelector } from "react-redux";
+const NavBar = () => {
+  const { totalCount } = useSelector((state) => state.cart);
+  return (
+    <>
+      <MDBNavbar expand="lg" dark bgColor="primary">
+        <MDBContainer>
+          <MDBNavbarBrand
+            style={{
+              alignItems: "center",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            Redux Toolkit shopping Cart{" "}
+          </MDBNavbarBrand>
+          <MDBNavbarLink>
+            <a href="" className="mx-3">
+            <MDBIcon fas icon="shopping-cart" size="lg" color="white" >
+            <MDBBadge
+            pill
+            color="danger"
+            notification
+            style={{ fontSize: "12px" }}
+          >
+          {totalCount}
+                </MDBBadge>
+              </MDBIcon>
+            </a>
+          </MDBNavbarLink>
+        </MDBContainer>
+      </MDBNavbar>
+    </>
+  );
+};
+
+export default NavBar;
